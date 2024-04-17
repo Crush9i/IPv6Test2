b78cd9429b91b8a4390d71c019f527e2cb4685c9
Rachel <56282761+Rachel-joy-4@users.noreply.github.com>
2024-04-17 11:40:04 +0800

    Add files via upload
    新增文本相似度保存和查询以及获取数据库中所有域名

--- sql/support_degree_database.py
+++ sql/support_degree_database.py
@@ -4,7 +4,7 @@ import json
 import pymysql
 
 import sql.ConnectDB as connDB
-
+from similarity.textSimilarity import calculate_sourcecode_similarity
 
 def insert_support_degree(conn, domain='www.baidu.com', resolved=False, accessed=False, support_degree=95.09,
                           connectivity: str = None,
@@ -43,12 +43,13 @@ def insert_support_degree(conn, domain='www.baidu.com', resolved=False, accessed
         ipv6_authorization_system,
         start_time, end_time, text_similarity, pic_similarity, text_structure_similarity)
     result = query_ipv6_records(conn, domain)
-    if result is not None:
+    if result:
         update_support_degree(conn, domain, resolved, accessed, support_degree, connectivity, secondary_connectivity,
                               tertiary_connectivity, resolve_delay, tcp_establishment_resolution_delay,
                               server_responds_first_packet_delay, server_responds_first_page_delay, access_stability,
                               ipv6_authorization_system, start_time, end_time, text_similarity, pic_similarity,
                               text_structure_similarity)
+        print("完成更新!")
     else:
         sql = """INSERT INTO ipv6_support_degree (domain, resolved, accessed, support_degree, connectivity,
                secondary_connectivity, tertiary_connectivity, resolve_delay, tcp_establishment_resolution_delay, 
@@ -60,6 +61,7 @@ def insert_support_degree(conn, domain='www.baidu.com', resolved=False, accessed
         cursor.execute(sql, data_tuple)
         conn.commit()
         cursor.close()
+        print("完成插入!")
 
 
 def query_ipv6_records(conn: pymysql.connections.Connection, domain='www.google.com'):
@@ -129,9 +131,56 @@ def update_support_degree(conn, domain='www.google.com', resolved=False, accesse
     conn.commit()
     cursor.close()
 
+#保存文本相似度到数据库
+def save_text_similarity(conn: pymysql.connections.Connection, domain='www.tsinghua.edu.cn'):
+    cursor = conn.cursor()
+    query_sql = "SELECT ipv4_source_code,ipv6_source_code FROM website_information WHERE domain = %s"
+    cursor.execute(query_sql, domain)
+    result = cursor.fetchall()
+    for tuple_element in result:
+        if tuple_element[0] and tuple_element[1]:
+            text_similarity = calculate_sourcecode_similarity(tuple_element[0], tuple_element[1])
+        else:
+            text_similarity=0
+        save_sql = """INSERT INTO ipv6_support_degree (domain,text_similarity) VALUES (%s, %s) ON DUPLICATE KEY UPDATE text_similarity = VALUES(text_similarity)"""
+        cursor.execute(save_sql, (domain,text_similarity))
 
-# if __name__ == '__main__':
-#     conn = connDB.get_mysql_conn()
-#     # insert_support_degree(conn)
-#     query_ipv6_records(conn)
-#     conn.close()
+#从数据库中查询文本相似度
+def query_text_similarity(conn: pymysql.connections.Connection, domain='www.tsinghua.edu.cn'):
+    print("域名：",domain)
+    cursor = conn.cursor()
+    query_sql = "SELECT text_similarity FROM ipv6_support_degree WHERE domain = %s"
+    cursor.execute(query_sql, domain)
+    result = cursor.fetchall()
+    cursor.close()
+    # print(result)
+    return result
+
+#对数据库中已有的所有domain的文本相似度进行保存和查询测试
+def test_text_similaryt(conn):
+    domain_list=get_all_domains_in_database(conn)
+    for domain in domain_list:
+        save_text_similarity(conn,domain)
+        query_text_similarity(conn,domain)
+        print("完成1条测试!")
+
+#获得数据库中所有域名domain
+def get_all_domains_in_database(conn):
+    cursor = conn.cursor()
+    query_sql = "SELECT domain FROM website_information;"
+    cursor.execute(query_sql)
+    result_tuple = cursor.fetchall()
+    domain_list=[]
+    for result in result_tuple:
+        domain=result[0]
+        domain_list.append(domain)
+    return domain_list
+
+if __name__ == '__main__':
+    conn = connDB.get_mysql_conn()
+    # insert_support_degree(conn)
+    test_text_similaryt(conn)
+    # save_text_similarity(conn)
+    # query_text_similarity(conn)
+    # query_ipv6_records(conn)
+    conn.close()
